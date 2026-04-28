f6c7d6099e5e5512d61053032e4d01d8ca952b40
VitorTxe <vitormateus1996@hotmail.com>
2026-04-28 02:05:38 -0300

    Transformando o origin em um array para aceitar as duas ulrs

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 // Configuração de segurança do CORS
 const corsOptions = {
-  origin: process.env.FRONTEND_URL || "http://localhost:5173", // Permite apenas o seu frontend (ajuste a porta se necessário)
+  origin: [process.env.FRONTEND_URL, "http://localhost:5173"], // Permite o frontend de produção E o localhost
   methods: ["GET", "POST", "PUT", "DELETE"], // Métodos HTTP permitidos
   allowedHeaders: ["Content-Type", "Authorization"] // Cabeçalhos permitidos
 };
